f1b5fe2ce03887e3ac1d6697a4d0bae02aa51d04
Heikki Holopainen <heikki.holopainen@gmail.com>
2017-03-04 12:00:27 +0200

    refactor: fix shellcheck warnings

--- do_dns_updater.sh
+++ do_dns_updater.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 set -euo pipefail
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+
 function cleanup() {
   echo "An error occured, cleaning up, removing ${FILE}"
-  rm -f ${FILE}
+  rm -f "${FILE}"
 }
 
 trap cleanup ERR
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )".
-source settings
+source settings # Settings file name
 
 FILE="${HOST}.ip"
-touch ${FILE}
+touch "${FILE}"
 
 IP=$(curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+')
 
-if cat ${FILE} | grep -q ${IP}; then
+if grep -q "${IP}" "${FILE}"; then
   echo "IP unchanged (${IP}), exiting!"
   exit 0
 fi
 
 JSON=$(curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v3/domains/$DOMAIN/records" | python -m json.tool)
 
-if echo ${JSON} | grep -q ${IP}; then
+if echo "${JSON}" | grep -q "${IP}"; then
   echo "IP found from JSON, no need to update!"
 else
   curl -s -X PUT -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -d "{\"data\":\"$IP\"}" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID" > /dev/null
 fi
 
 echo "Saving IP ${IP} to file ${FILE}"
-echo $IP > ${FILE}
+echo "${IP}" > "${FILE}"
 
 exit 0
