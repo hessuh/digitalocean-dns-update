b424b4b62d15efbaf895615144171f1d99823523
Heikki Holopainen <heikki.holopainen@gmail.com>
2017-03-04 11:38:54 +0200

    Initial commit

---
+++ do_dns_updater.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -euo pipefail
+
+function cleanup() {
+  echo "An error occured, cleaning up, removing ${FILE}"
+  rm -f ${FILE}
+}
+
+trap cleanup ERR
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )".
+source settings
+
+FILE="${HOST}.ip"
+touch ${FILE}
+
+IP=$(curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+')
+
+if cat ${FILE} | grep -q ${IP}; then
+  echo "IP unchanged (${IP}), exiting!"
+  exit 0
+fi
+
+JSON=$(curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v3/domains/$DOMAIN/records" | python -m json.tool)
+
+if echo ${JSON} | grep -q ${IP}; then
+  echo "IP found from JSON, no need to update!"
+else
+  curl -s -X PUT -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -d "{\"data\":\"$IP\"}" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID" > /dev/null
+fi
+
+echo "Saving IP ${IP} to file ${FILE}"
+echo $IP > ${FILE}
+
+exit 0
